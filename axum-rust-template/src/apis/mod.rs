165d89112a782210678ab803491fe804bf4b319b
noobHuKai <hukai2943@aliyun.com>
2023-02-22 21:27:20 +0800

    refactor

--- axum-rust-template/src/apis/mod.rs
+++
@@ -1,5 +0,0 @@
-use axum::response::Html;
-
-pub async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
-}
--- axum-rust-template/src/router.rs
+++
@@ -1,9 +0,0 @@
-use axum::{routing::get, Router};
-
-use crate::apis;
-
-pub fn init_router() -> Router {
-    let router = Router::new().route("/", get(apis::handler));
-
-    router
-}
